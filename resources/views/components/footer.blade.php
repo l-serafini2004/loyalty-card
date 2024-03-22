5dd9bf700e0f8f1c2173dd3e4e2ef83c4b2d1767
Luca Serafini <serafiniluca2004@gmail.com>
2024-03-22 22:15:04 +0100

    Hompage, footer (without navbar)

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,28 @@
+<head>
+    <link rel="stylesheet" href="../style/components/footer.css">
+</head>
+
+<footer>
+    <div class="foot">
+        <h3>Contact</h3>
+        <p><a href="#">Email</a></p>
+        <p><a href="#">My Website</a></p>
+    </div>
+
+    <div class="foot">
+        <h3>How to use it</h3>
+        <p><a href="#">APIs integration</a></p>
+        <p><a href="#">GitHub Page</a></p>
+        <p><a href="#">Documentation</a></p>
+        <p><a href="#">Plans info</a></p>
+    </div>
+
+    <div class="foot">
+        <h3>Legal</h3>
+        <p><a href="#">Cookies</a></p>
+        <p><a href="#">License of use</a></p>
+        <p><a href="#">Privacy info</a></p>
+        <p><a href="#">Copyright@2024</a></p>
+    </div>
+
+</footer>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome On LoyaltyCard</title>
+    <link rel="stylesheet" href="style/index.css">
+</head>
+<body>
+<nav>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Documentation</a></li>
+        <li><a href="#">Dashboard</a></li>
+    </ul>
+</nav>
+<main>
+    <header>
+        <div class="write">
+            <h1>Welcome on LoyaltyCard</h1>
+            <p>Where you can create your personalized Fidelity Card plan for your activity</p>
+
+            <div class="bttns">
+                <p><a href="#">Register</a></p>
+                <p><a href="#">Your activity</a></p>
+            </div>
+
+        </div>
+        <div class="card">
+
+            <div class="example">
+                <h4>Your Shop</h4>
+                <div class="card-info">
+                    <p>Mario Rossi</p>
+                    <p>9982732934</p>
+                    <p>Mariorossi@mail.com</p>
+                    <p>12349837283294423</p>
+                </div>
+            </div>
+            <h3>Example of a card</h3>
+        </div>
+    </header>
+
+
+    <article class="descriptions">
+        <div class="par">
+            <h2>What's LoyaltyCard?</h2>
+            <p>
+                LoyaltyCard is a website that allows you to improve your activity with an efficient loyalty card service.
+                With the dashboard of this website you can manage all your customers and with our APIs you can add to your website
+                a page in order your client could manage their electronic LoyaltyCard.
+                You can select your favourite plan (it's depends on your need).
+            </p>
+        </div>
+        <div class="par right">
+            <h2>Why you should choose us?</h2>
+            <p>
+                LoyaltyCard is a website that allows you to improve your activity with an efficient loyalty card service.
+                With the dashboard of this website you can manage all your customers and with our APIs you can add to your website
+                a page in order your client could manage their electronic LoyaltyCard.
+                You can select your favourite plan (it's depends on your need).
+            </p>
+        </div>
+    </article>
+
+    <article class="maps">
+        <h2>Shops who use FidelityCard</h2>
+        <img src="https://www.ionos.it/digitalguide/fileadmin/DigitalGuide/Screenshots_2023/google-my-maps.png">
+    </article>
+
+    <h2 class="plans">Our Plans</h2>
+    <article class="prices">
+        <div class="select-abb">
+            <div class="info-select">
+                <h2>Free Plan</h2>
+                <p class="descp">Perfect for your familiar run business</p>
+                <p class="price">0,00</p>
+                <a href="#">Subscribe</a>
+            </div>
+            <hr>
+            <p class="srvc">Services</p>
+            <ul>
+                <li>
+                    <b>500</b> card for your customers
+                </li>
+                <li>
+                    1 <b>type of card</b>
+                </li>
+                <li>
+                    1 <b>account</b>
+                </li>
+                <li>
+                    <b>Free APIs</b> services
+                </li>
+            </ul>
+        </div>
+        <div class="select-abb">
+            <div class="info-select">
+                <h2>Premium Plan</h2>
+                <p class="descp">Perfect for your medium activity</p>
+                <p class="price">3,99</p>
+                <a href="#">Subscribe</a>
+            </div>
+            <hr>
+            <p class="srvc">Services</p>
+            <ul>
+                <li>
+                    <b>1500</b> card for your customers
+                </li>
+                <li>
+                    2 <b>type of cards</b>
+                </li>
+                <li>
+                    5 <b>accounts</b>
+                </li>
+                <li>
+                    <b>Free APIs</b> services
+                </li>
+            </ul>
+        </div>
+        <div class="select-abb">
+            <div class="info-select">
+                <h2>Business Plan</h2>
+                <p class="descp">Perfect for your large activity</p>
+                <p class="price">8,99</p>
+                <a href="#">Subscribe</a>
+            </div>
+            <hr>
+            <p class="srvc">Services</p>
+            <ul>
+                <li>
+                    <b>5000</b> card for your customers
+                </li>
+                <li>
+                    3 <b>type of cards</b>
+                </li>
+                <li>
+                    15 <b>accounts</b>
+                </li>
+                <li>
+                    <b>Free APIs</b> services
+                </li>
+            </ul>
+        </div>
+    </article>
+
+</main>
+<x-footer></x-footer>
+
+</body>
+</html>
